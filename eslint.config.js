cbdff8def3f819f0ffdb866f84b8fb22967a981b
Kevin T <30321316+glaciyan@users.noreply.github.com>
2024-10-12 10:32:01 +0200

    configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,70 @@
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import vuePlugin from 'eslint-plugin-vue';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import vueEslintParser from 'vue-eslint-parser';
+
+export default [
+  ...vuePlugin.configs['flat/recommended'],
+  {
+    files: ["**/*.ts", "**/*.vue"],
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        parser: typescriptEslintParser,
+        sourceType: "module",
+        ecmaVersion: 2021,
+        extraFileExtensions: [".vue"],
+      },
+      globals: {
+        node: true,
+        browser: true,
+        es2021: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      "vue": vuePlugin,
+    },
+    rules: {
+      // Vue specific rules
+      "vue/attribute-hyphenation": [
+        "error",
+        "never",
+        {
+          ignore: [],
+        },
+      ],
+      "vue/v-on-event-hyphenation": [
+        "error",
+        "never",
+        {
+          autofix: false,
+          ignore: [],
+        },
+      ],
+      "vue/multi-word-component-names": [
+        "error",
+        {
+          ignores: ["Center", "Modal", "Builds", "Index"],
+        },
+      ],
+
+      // TypeScript specific rules
+      "@typescript-eslint/no-explicit-any": ["off"],
+      "@typescript-eslint/no-empty-interface": ["off"],
+      "@typescript-eslint/no-non-null-assertion": ["off"],
+      "@typescript-eslint/explicit-module-boundary-types": ["warn"],
+
+      // Custom rules
+      "no-console": "warn",
+      "no-debugger": "error",
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".vue"],
+        },
+      },
+    },
+  },
+];
