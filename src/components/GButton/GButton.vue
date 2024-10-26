f01dc26b456ba3bd86bfbb0053cd0d26b5c8a36f
Kevin T <30321316+glaciyan@users.noreply.github.com>
2024-10-26 17:17:12 +0200

    fix import

--- src/components/GButton/GButton.vue
+++ src/components/GButton/GButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ButtonLoadingPosition } from "./ButtonLoadingPosition";
-import GSpinner from "../GSpinner.vue";
+import GSpinner from "~/components/GSpinner.vue";
 
 export interface Props {
   /** The text that will be displayed on the button. */
