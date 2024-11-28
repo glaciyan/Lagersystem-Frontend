4c12959560efe6bb624a0a0e515ac9052b0f5d54
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2024-11-28 16:34:28 +0100

    Revert "Form Manager"

--- src/components/Form/LinkedForm.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const submit = inject("submit") as () => any;
-</script>
-
-<template>
-  <form @submit.prevent="submit">
-    <slot />
-  </form>
-</template>
--- src/components/Form/form.ts
+++
@@ -1,9 +0,0 @@
-export function form<T extends {}>() {
-    type Errors = Record<keyof T, string | undefined>;
-    type Form = {
-      values: T;
-      errors: Errors;
-    };
-
-    return ref<Form>({} as Form);
-};
