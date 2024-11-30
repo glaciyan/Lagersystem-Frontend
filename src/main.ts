963f239368198f51f0cb518a3b97c7f67c049af0
Kevin T <30321316+glaciyan@users.noreply.github.com>
2024-11-30 20:59:16 +0100

    clean up main

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,8 @@ import "./styles/main.less";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { endpoints } from "./lib/api/endpoints";
-import { api } from "./lib/api/api";
-import { match } from "./lib/api/match";
 
 const app = createApp(App);
 
 app.use(router);
 app.mount("#app");
-
-const result = await api(endpoints.getStorages, {});
-match(result, {
-  ok: (data) => {
-    console.log(data);
-  },
-  error: (err) => {
-    console.log(err);
-  },
-});
