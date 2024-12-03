00316510b2276127fbff2c29e7fa14b6811bedfd
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2024-12-03 14:37:34 +0100

    improve use api

--- src/lib/api/useApi.ts
+++ src/lib/api/useApi.ts
@@ -1,6 +1,7 @@
 import { ZodSchema, ZodTypeAny } from "zod";
 import { api } from "./api";
-import { ApiResult, BasicEndpoint, Body, Input, Params, Query } from "./core";
+import { ApiError, ApiResult, BasicEndpoint, Body, Input, Params, Query } from "./core";
+import { match } from "./match";
 
 export function useApi<Q extends Query = {},
   P extends Params = {},
@@ -8,10 +9,26 @@ export function useApi<Q extends Query = {},
   Ret = {},
   Zod extends ZodSchema = ZodTypeAny>(endpoint: BasicEndpoint<Q, P, B, Ret, Zod>, input: Input<Q, P, B>, init?: RequestInit) {
   const result = ref<ApiResult<Ret> | null>(null);
+  const data = ref<Ret | null>(null);
+  const errors = ref<ApiError[] | null>(null);
+  const loading = ref(true);
 
   api(endpoint, input, init).then((out) => {
     result.value = out;
   });
 
-  return result;
+  watchEffect(() => {
+    if (result.value) {
+      loading.value = false;
+      match(result.value, {
+        ok: val => data.value = val,
+        error: err => errors.value = err,
+      });
+    }
+    else {
+      loading.value = true;
+    }
+  });
+
+  return { data, errors, loading };
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import GButton from "~/components/Button/BaseButton.vue";
 import PageContainer from "~/components/PageContainer";
+import { endpoints } from "~/lib/api/config/endpoints";
+import { useApi } from "~/lib/api/useApi";
+
+const { data, errors, loading } = useApi(endpoints.getStorages, {});
 </script>
 
 <template>
+  <div v-if="loading">
+    <p>Loading...</p>
+  </div>
+  <div v-else>
+    <p v-if="errors !== null">
+      Something went wrong!
+    </p>
+    <p
+      v-for="storage of data"
+      v-else
+      :key="storage.id"
+    >
+      {{ storage.name }} - {{ storage.id }}
+    </p>
+  </div>
   <PageContainer>
     <GButton
       label="hello"
