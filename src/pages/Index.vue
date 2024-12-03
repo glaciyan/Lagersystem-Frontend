f3a7ad0d0829d222d5404828ca89db38835cbe74
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2024-12-03 14:26:31 +0100

    remove example

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import PageContainer from "~/components/PageContainer";
 import GButton from "~/components/Button/BaseButton.vue";
-import ApiForm from "~/components/Form/ApiForm.vue";
-import { endpoints } from "~/lib/api/config/endpoints";
-import FormInput from "~/components/Form/FormInput.vue";
-import { Button } from "ant-design-vue";
+import PageContainer from "~/components/PageContainer";
 </script>
 
 <template>
@@ -17,22 +13,4 @@ import { Button } from "ant-design-vue";
       loadingPosition="left"
     />
   </PageContainer>
-  <ApiForm
-    :endpoint="endpoints.postStorage"
-    :initialState="{ name: 'My Name is', description: 'Description 1010101' }"
-    @success="(data) => console.log('worked', data)"
-    @failure="(err) => console.log('failed', err)"
-  >
-    <FormInput
-      for="name"
-      type="text"
-    />
-    <FormInput
-      for="description"
-      type="text"
-    />
-    <Button htmlType="submit">
-      Submit
-    </Button>
-  </ApiForm>
 </template>
