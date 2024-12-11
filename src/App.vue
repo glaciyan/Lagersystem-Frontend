a4570bc4de32b33a7906d8ecb3a40d6271d9101e
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2024-12-11 11:05:27 +0100

    antd styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import { App, ConfigProvider, StyleProvider, theme } from "ant-design-vue";
 import { RouterView } from "vue-router";
 import TheFooter from "~/components/TheFooter.vue";
 import TheHeader from "~/components/TheHeader.vue";
 </script>
 
 <template>
-  <TheHeader />
-  <main class="min-h-screen">
-    <RouterView />
-  </main>
-  <TheFooter />
+  <StyleProvider hashPriority="high">
+    <ConfigProvider
+      :theme="{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          fontSize: 15,
+          colorPrimary: '#02b1e7',
+          colorInfo: '#37c7fe',
+        }
+      }"
+    >
+      <App>
+        <TheHeader />
+        <main class="min-h-screen">
+          <RouterView />
+        </main>
+        <TheFooter />
+      </App>
+    </ConfigProvider>
+  </StyleProvider>
 </template>
