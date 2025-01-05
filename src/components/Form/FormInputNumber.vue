7fef4a146df979e3d04f441f831dd0b753906895
willstef95 <stefan-willmann95@web.de>
2025-01-05 14:04:37 +0100

    FormInputs angepasst und erweitert

---
+++ src/components/Form/FormInputNumber.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { InputProps } from "ant-design-vue";
+import { InputNumber } from "ant-design-vue";
+import { inject } from "vue";
+
+const props = defineProps<{ for: string; placeholder?: string } & InputProps>();
+const values = inject("values") as Ref<Record<string, any>>;
+
+// Sicherstellen, dass der initiale Wert existiert und eine Zahl ist
+if (!values.value[props.for]) {
+  values.value[props.for] = 0; // Initialisieren mit 0, falls nicht vorhanden
+}
+</script>
+
+<template>
+  <InputNumber v-model:value="values[props.for]" :placeholder="props.placeholder" :step="0.01" style="width: 100%" />
+</template>
--- src/components/Form/FormInputTextArea.vue
+++ src/components/Form/FormInputTextArea.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import { Textarea, type InputProps } from "ant-design-vue";
 
-const props = defineProps<{ for: string } & InputProps>();
+const props = defineProps<{ for: string; placeholder?: string } & InputProps>();
 const values = inject("values") as Ref<any>;
 </script>
 
 <template>
-  <Textarea
-    v-model:value="values[props.for]"
-    v-bind="$attrs"
-    placeholder="Please enter your description"
-  />
+  <Textarea v-model:value="values[props.for]" v-bind="$attrs" :placeholder="props.placeholder" />
 </template>
