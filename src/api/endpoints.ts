6ae4de0ec8fef7697b320fb18f68a76c0db0dbfa
Kevin Trautvetter <30321316+glaciyan@users.noreply.github.com>
2025-02-12 08:39:06 +0100

    update endpoints

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -12,16 +12,19 @@ export const endpoints = {
   postStorage: endpoint("POST", "/v1/storages").withBody<{ name: string; description: string; parentId?: string }>().returns(Storage),
   deleteStorage: endpoint<{ id: string }>("DELETE", p => `/v1/storages/${p.id}`).returns(Storage),
   moveStorage: endpoint<{ id: string }>("PATCH", p => `/v1/storages/${p.id}/move`).withBody<{ newParentId: string }>().returns(Storage),
+  updateStorage: endpoint<{ id: string }>("PATCH", p => `/v1/storages/${p.id}/update`).withBody<{ name: string; description: string }>().returns(Storage),
 
   // Spaces
   postSpace: endpoint("POST", "/v1/spaces").withBody<{ name: string; totalSize: number; unit: string; description: string; storageId: string }>().returns(Space),
   deleteSpace: endpoint<{ id: string }>("DELETE", p => `/v1/spaces/${p.id}`).returns(Space),
   moveSpace: endpoint<{ id: string }>("PATCH", p => `/v1/spaces/${p.id}/move`).withBody<{ targetStorageId: string }>().returns(Space),
+  updateSpace: endpoint<{ id: string }>("PATCH", p => `/v1/spaces/${p.id}/update`).withBody<{ name: string; totalSize: number; description: string }>().returns(Space),
 
   // Products
   postProduct: endpoint("POST", "/v1/products").withBody<{ name: string; description: string; unit: string; size: number }>().returns(Product),
   getProducts: endpoint("GET", `/v1/products`).returns(ProductArray),
   deleteProduct: endpoint<{ id: string }>("DELETE", p => `/v1/products/${p.id}`).returns(Product),
+  updateProduct: endpoint<{ id: string }>("PATCH", p => `/v1/products/${p.id}/update`).withBody<{ name: string; description: string; size: number }>().returns(Product),
 
   // Stored Products
   getStoredProductsFromSpace: endpoint<{ id: string }>("GET", p => `/v1/storedProducts/${p.id}`).returns(StoredProductArray),
